04abd8e7b0ed66675510032d54fecb3ed5eabdef
Rahul Biswas <rbiswas1@uw.edu>
2018-08-25 22:50:19 -0700

    updated function

--- Rahul/get_highfire_starts.py
+++ Rahul/get_highfire_starts.py
@@ -1,12 +1,8 @@
 def get_highfire_starts(sdf):
-    h=25;
-    sgma=h/2;
-    conv_fn=stats.norm.pdf(np.arange(-25,25)/sgma)
-    sdf_trend=np.convolve(sdf);
-    sdf_max=np.max(sdf_trend);
-    sdf_thresh=(25.0/100)*sdf_max;
-    mask=np.where(sdf_trend<=sdf_thresh);
-    sdf_trend_subset=sdf_trend[mask];
+    sdf=sdf_out;
+    sdf_trend=sdf;
+    sdf_max_idx=np.argmax(sdf_trend);
+    sdf_trend_subset=sdf_trend[:sdf_max_idx];
     thresh = -10000;
     minima_idx=argrelextrema(sdf_trend_subset, np.less)[0];
     i=1;
@@ -14,7 +10,7 @@ def get_highfire_starts(sdf):
     while sdf_trend_subset[out_idx]>thresh:
         out_idx_old=out_idx;
         sdf_sub_2 = sdf[:out_idx];
-        thresh=np.quantile(sdf_sub_2,0.75)
+        thresh=np.percentile(sdf_sub_2,75)
         sdf_trend_subset = sdf_sub_2;
         i+=1;
         out_idx=minima_idx[len(minima_idx)-i];
