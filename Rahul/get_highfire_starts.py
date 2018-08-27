acf98ba927cb63347dc5646fed8fdee176fca73d
Rahul Biswas <rbiswas1@uw.edu>
2018-08-26 23:33:43 -0700

    threshold depends on baseline

--- Rahul/get_highfire_starts.py
+++ Rahul/get_highfire_starts.py
@@ -3,22 +3,20 @@ def get_highfire_starts(sdf,pre_stim_time,min_start_time):
     This function estimates and returns the first time (in miliseconds) the neuron starts high-firing.
     Input : An array with spike density function values over each milisecond,
             Time (in seconds) until stimulus onset,
-            Minimum latency after stimulus onset
+            Minimum latency (in seconds) after stimulus onset
     Output : Index in sdf array where high-firing after stimulus starts
     '''
     from scipy.signal import argrelextrema
     from scipy.stats import iqr
-    sdf_stim=sdf[(int(1000*(pre_stim_time))+5):]
+    sdf_stim=sdf[(int(1000*(pre_stim_time))+min_start_time):]
     sdf_max_idx=np.argmax(sdf_stim)
-    sdf_max_idx_all=argrelextrema(sdf_stim,np.greater)[0]
+    baseline = sdf[(int(1000*(pre_stim_time))-20):(int(1000*(pre_stim_time))+20)]
+    thresh0=np.mean(baseline) + 2* np.std(baseline)
+    sdf_max_idx_all=argrelextrema(sdf_stim, np.greater_equal,order=2)[0]
     if len(sdf_max_idx_all)==0:
         return min_start_time;
     
-    thresh0=np.percentile(sdf_stim[sdf_max_idx_all],75)+1.5*iqr(sdf_stim[sdf_max_idx_all]);
-    if np.all(sdf_stim[sdf_max_idx]<thresh0):
-        sdf_min_idx=np.argmin(sdf_stim)
-        sdf_min_idx_all=argrelextrema(sdf_stim,np.greater)[0]
-        
+    if np.all(sdf_stim[sdf_max_idx_all]<thresh0):
         return min_start_time;
     
     sdf_max_idx=np.min(sdf_max_idx_all[sdf_stim[sdf_max_idx_all]>=thresh0])
@@ -29,28 +27,19 @@ def get_highfire_starts(sdf,pre_stim_time,min_start_time):
             break;
         else:
             out_idx=find_min_highfire(sdf_stim_subset);
-#         i=1;
-#         while sdf_stim_subset[out_idx]>thresh:
-#             relmaxima_temp=argrelextrema(sdf_stim[:out_idx_old], np.greater)[0];
-#             if len(relmaxima_temp)>0:
-#                 idx_temp0=relmaxima_temp[-1]
-#                 if idx_temp0==0:
-#                     out_idx=0;
-#                     break;
-#                 else:
             sdf_stim_subset2 = sdf_stim[:out_idx];
             thresh=np.percentile(sdf_stim_subset2,75)
             if sdf_stim_subset[out_idx]<thresh:
                 break;
             else:
-                sdf_max_idx2_all=argrelextrema(sdf_stim_subset2,np.greater)[0]
+                sdf_max_idx2_all=argrelextrema(sdf_stim_subset2, np.greater_equal,order=2)[0]
                 if len(sdf_max_idx2_all)==0:
                     break;
                 sdf_max_idx2=sdf_max_idx2_all[-1]
                 sdf_stim_subset2=sdf_stim_subset[:sdf_max_idx2]
                 sdf_stim_subset=sdf_stim_subset2;
             
-    idx_temp=argrelextrema(sdf_stim[out_idx:], np.greater)[0][0]
+    idx_temp=argrelextrema(sdf_stim[out_idx:],np.greater_equal,order=2)[0][0]
     out_idx=out_idx+(idx_temp/2);
     out_idx=out_idx+min_start_time;
     return out_idx
