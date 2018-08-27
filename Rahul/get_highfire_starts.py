976d12eaf21952633816173a463d46caf82a9505
Rahul Biswas <rbiswas1@uw.edu>
2018-08-26 17:25:43 -0700

    updated function

--- Rahul/get_highfire_starts.py
+++ Rahul/get_highfire_starts.py
@@ -1,28 +1,53 @@
-def get_highfire_starts(sdf,pre_stim_time):
+def get_highfire_starts(sdf,pre_stim_time,min_start_time):
     '''
-    This function accepts an array with spike density function values over each milisecond,
-    and the time (in seconds) until stimulus onset.
-    It estimates and returns the first time (in miliseconds) the neuron started high-firing.
+    This function estimates and returns the first time (in miliseconds) the neuron starts high-firing.
+    Input : An array with spike density function values over each milisecond,
+            Time (in seconds) until stimulus onset,
+            Minimum latency after stimulus onset
+    Output : Index in sdf array where high-firing after stimulus starts
+    
     '''
-    sdf_stim=sdf[int(1000*pre_stim_time):]
+    #    from scipy import signal
+    from scipy.stats import iqr
+    sdf_stim=sdf[(int(1000*(pre_stim_time))+5):]
     sdf_max_idx=np.argmax(sdf_stim)
-    sdf_stim_subset=sdf_stim[:sdf_max_idx];
-    thresh = -10000;
-    minima_idx=argrelextrema(sdf_stim_subset, np.less)[0];
-    if not minima_idx:
-        out_idx=0;
-        final_chunk = sdf_stim[out_idx:]
-        out_idx=out_idx+np.where(final_chunk>final_chunk[0])[0][0]-1
-    else:
-        i=1;
-        out_idx=minima_idx[len(minima_idx)-i];
-        while sdf_stim_subset[out_idx]>thresh:
-            out_idx_old=out_idx;
-            sdf_sub_2 = sdf[:out_idx];
-            thresh=np.percentile(sdf_sub_2,75)
-            sdf_stim_subset = sdf_sub_2;
-            i+=1;
-            out_idx=minima_idx[len(minima_idx)-i];
+    sdf_max_idx_all=argrelextrema(sdf_stim,np.greater)[0]
+    if len(sdf_max_idx_all)==0:
+        return min_start_time;
+    if sdf_stim[sdf_max_idx]<np.median(sdf_stim[sdf_max_idx_all])+2*np.std(sdf_stim[sdf_max_idx_all]):
+        return min_start_time;
+    thresh0=np.percentile(sdf_stim[sdf_max_idx_all],75)+1.5*iqr(sdf_stim[sdf_max_idx_all]);
+    sdf_max_idx=np.min(sdf_max_idx_all[sdf_stim[sdf_max_idx_all]>=thresh0])
+    
+    sdf_stim_subset=sdf_stim[:sdf_max_idx];out_idx=0;
+    while True:
+        if len(sdf_stim_subset)<3:
+            break;
+        else:
+            out_idx=find_min_highfire(sdf_stim_subset);
+#         i=1;
+#         while sdf_stim_subset[out_idx]>thresh:
+#             relmaxima_temp=argrelextrema(sdf_stim[:out_idx_old], np.greater)[0];
+#             if len(relmaxima_temp)>0:
+#                 idx_temp0=relmaxima_temp[-1]
+#                 if idx_temp0==0:
+#                     out_idx=0;
+#                     break;
+#                 else:
+            sdf_stim_subset2 = sdf_stim[:out_idx];
+            thresh=np.percentile(sdf_stim_subset2,75)
+            if sdf_stim_subset[out_idx]<thresh:
+                break;
+            else:
+                sdf_max_idx2_all=argrelextrema(sdf_stim_subset2,np.greater)[0]
+                if len(sdf_max_idx2_all)==0:
+                    break;
+                sdf_max_idx2=sdf_max_idx2_all[-1]
+                sdf_stim_subset2=sdf_stim_subset[:sdf_max_idx2]
+                sdf_stim_subset=sdf_stim_subset2;
+            
     idx_temp=argrelextrema(sdf_stim[out_idx:], np.greater)[0][0]
     out_idx=out_idx+(idx_temp/2);
+    out_idx=out_idx+min_start_time;
+    return out_idx
     return out_idx
